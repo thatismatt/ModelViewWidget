86de3be7def7ca3ad964ce21aabb01fb4296bbe5
Matt Lee <thatismatt@gmail.com>
2012-10-20 13:45:14 +0100

    Highlight doc headings.

--- doc.js
+++ doc.js
@@ -4,11 +4,22 @@ function isComment(l) {
     return l.trim().substring(0, 2) == '//';
 }
 
+function isTitle(l) {
+    return l.indexOf('#') > -1;
+}
+
 $.get('mvw.js', function (raw) {
     var lines =
         $(raw.split('\n'))
-            .map(function(i, l) { return l.length === 0 ? ' ' : l; })
-            .map(function(i, l) { return isComment(l) ? '<div class="doc-line">' + l + '</div>' : l; })
+            .map(function(i, l) {
+                return l.length === 0 ? ' ' : l;
+            })
+            .map(function(i, l) {
+                var type = isTitle(l) ? 'title' : 'line';
+                return isComment(l)
+                    ? '<div class="doc ' + type + '">' + l + '</div>'
+                    : l;
+            })
             .toArray();
     var html =
         '<pre><div>' +
